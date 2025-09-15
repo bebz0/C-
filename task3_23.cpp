a35fb73bf0c3d9b344e5c1e3c7d559b3f3083325
bebz0 <romanbizonua@gmail.com>
2025-09-15 22:44:21 +0300

    task3_23 changed

--- task3_23.cpp
+++ task3_23.cpp
@@ -1,43 +1,77 @@
 #include <iostream>
 #include <cmath>
+#include <limits> // for inf
 
 using namespace std;
 
+// Soft Exponential
 double softExponential(double a, double x)
 {
-
-    if (a < 0) // a<0
+    if (a < 0) // a < 0
     {
-        if (1 - a * (x + a) <= 0)
+        double arg = 1 - a * (x + a);
+        if (arg < 0) // log argument cannot be negative
         {
-            cout << "log argument <= 0" << endl;
+            cout << "log argument < 0,  undefined" << endl;
             return NAN;
-            return -log(1 - a * (x + a)) / a;
         }
+        if (fabs(arg) < 1e-15) // argument too close to zero
+        {
+            cout << "function -> max double" << endl;
+            return numeric_limits<double>::max();
+        }
+        return -log(arg) / a;
     }
-
-    if (a == 0) // a==0
+    else if (a == 0) // a = 0
+    {
         return x;
-
-    else // a>0
-        return (exp(a * x) - 1) / a + a;
+    }
+    else // a > 0
+    {
+        double val = (exp(a * x) - 1) / a + a;
+        // if overflw
+        if (val > numeric_limits<double>::max())
+        {
+            cout << "function -> max double" << endl;
+            return numeric_limits<double>::max();
+        }
+        return val;
+    }
 }
 
+// Der of Soft Exponential
 double der_SoftExponential(double a, double x)
 {
-    if (a < 0)
+    if (a < 0) // a < 0
     {
-        if (1 - a * (x + a) <= 0)
+        double denom = 1 - a * (x + a);
+        if (fabs(denom) < 1e-15) // denom close to zero
         {
-            cout << "deerivative undefined" << endl;
+            cout << "derivative -> max double" << endl;
+            return numeric_limits<double>::max();
+        }
+        if (denom < 0) // undefined for negative denom
+        {
+            cout << "derivative undefinedd" << endl;
             return NAN;
         }
-        return 1 / (1 - a * (x + a));
+        return 1.0 / denom;
     }
-    else if (a == 0)
-        return 1;
-    else // a > 0
-        return exp(a * x);
+    else if (a == 0) // a = 0
+    {
+        return 1.0;
+    }
+    else // case a > 0
+    {
+        double val = exp(a * x);
+        //  overflow
+        if (val > numeric_limits<double>::max())
+        {
+            cout << "derivative -> max double" << endl;
+            return numeric_limits<double>::max();
+        }
+        return val;
+    }
 }
 
 int main()
